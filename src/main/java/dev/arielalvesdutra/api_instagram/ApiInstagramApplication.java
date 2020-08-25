6c923f7b063aacab130596fc472b2dc5c6fb3c45
Ariel Alves Dutra <alves.lsn@gmail.com>
2020-08-25 18:19:09 -0300

    Initial commit

---
+++ src/main/java/dev/arielalvesdutra/api_instagram/ApiInstagramApplication.java
@@ -0,0 +1,13 @@
+package dev.arielalvesdutra.api_instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiInstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arielalvesdutra/api_instagram/ApiInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arielalvesdutra.api_instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
